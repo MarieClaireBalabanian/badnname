05290a98333e952d2490c38742f5d80a0423c8d6
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2016-07-15 10:08:06 -0500

    env fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var dotenv     = require('dotenv').config();
+var dotenv     = require('dotenv').config(),
     express    = require('express'),
     app        = express(),
     exphbs     = require('express-handlebars'),
